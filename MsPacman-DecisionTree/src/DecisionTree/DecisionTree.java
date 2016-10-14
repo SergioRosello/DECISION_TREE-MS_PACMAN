ad8c886bf83791e36129bfaf9499d9c8dea162ce
SergioRM-Patatita <sergio-rosello@hotmail.com>
2016-10-14 04:09:28 +0200

    Añadida la clase Nodo para crear el arbol de búsqueda.

--- MsPacman-DecisionTree/src/DecisionTree/DecisionTree.java
+++ MsPacman-DecisionTree/src/DecisionTree/DecisionTree.java
@@ -1,8 +1,36 @@
 package DecisionTree;
+import dataRecording.DataTuple;
+import dataRecording.Dataset;
+
+import java.util.List;
 
 /**
  * Created by ramonserranolopez on 6/10/16.
  */
 public class DecisionTree {
 
+    //Genera el arbol de decisiones según el dataset y la lista de atributos.
+    public void generateTree(Dataset trainingData, List<String> attrList){
+
+        //Creamos un Nodo.
+        Node node = new Node();
+
+        //2. Si las tuplas en dataset tienen todas la misma clase C, return N como un nodo hoja etiquetado con la clase C.
+
+
+        /*
+        *
+        String className;
+        if (sameClass(dataset)) {
+            className = dataset.get(0).DirectionChosen.toString();
+            n = new SimpleNode(className);
+            return n;
+        }
+        * */
+    }
+
+    //Función que calcula el mayor beneficio.
+    public String seleccionAtributos(){
+        return null;
+    }
 }
---
+++ MsPacman-DecisionTree/src/DecisionTree/Node.java
@@ -0,0 +1,11 @@
+package DecisionTree;
+
+/**
+ * Created by Sergio Roselló Morell on 10/14/2016.
+ */
+public class Node {
+    //Que va a tener el nodo de Java?
+    //Para poder implementar nuestro subDataSet.
+
+    //Tenemos una lista de hijos:
+}
